aa6241e77833bcfe78ed8b0198cf3c1cea57c9e8
Andrew Beresford <beezly@beezly.org.uk>
2013-03-25 18:32:10 +0000

    Use setup section for ensuring we have an api object available in tests

--- test/test_jive_api.rb
+++ test/test_jive_api.rb
@@ -1,23 +1,23 @@
 require 'test/unit'
 require 'jive_api'
 
+
 class TestJiveAPI < Test::Unit::TestCase
+  def setup
+    @url = ENV['JIVE_URL']
+    @user = ENV['JIVE_USER']
+    @pass = ENV['JIVE_PASS']
+    @api = Jive::Api.new @user, @pass, @url
+  end
+
   def test_version
-    url = ENV['JIVE_URL']
-    user = ENV['JIVE_USER']
-    pass = ENV['JIVE_PASS']
-    api = Jive::Api.new user, pass, url
     assert_nothing_raised do
-      api.api_version
+      @api.api_version
     end
   end
 
   def test_spaces
-    url = ENV['JIVE_URL']
-    user = ENV['JIVE_USER']
-    pass = ENV['JIVE_PASS']
-    j = Jive::Api.new user, pass, url
-    s = j.spaces :limit => 10
+    s = @api.spaces :limit => 10
     assert_operator s.count, :>, 1
   end
 end
