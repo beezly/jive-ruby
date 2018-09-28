d6f4c9301ea57635a9bf2d9877105508da11aa71
Nick Hammond <nick@nickhammond.com>
2018-09-28 13:59:37 -0500

    Tell Net::HTTP to use ssl when downloading the binary file, attr_reader for binary_url

--- lib/jive/gettable_binary_url.rb
+++ lib/jive/gettable_binary_url.rb
@@ -5,6 +5,7 @@ module Jive
     def get_binary
       binary_uri = URI @binary_url
       http = Net::HTTP.new(binary_uri.host, binary_uri.port)
+      http.use_ssl = true
       req = Net::HTTP::Get.new binary_uri.request_uri
       req.basic_auth @api_instance.auth[:username], @api_instance.auth[:password]
       response = http.request req
